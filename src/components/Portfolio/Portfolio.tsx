63e365d49cbb4651595e19e18d6bd2020ef2f23e
BogdanKonovalov21 <128465385+BogdanKonovalov21@users.noreply.github.com>
2023-06-22 18:24:18 +0300

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,11 +1,11 @@
-'use client';
-import { useState } from 'react';
-import Image from 'next/image';
-import image1 from '../../../public/images/portfolio/image1.png';
-import image2 from '../../../public/images/portfolio/image2.jpg';
-import image3 from '../../../public/images/portfolio/image3.jpg';
-import image4 from '../../../public/images/portfolio/image4.jpg';
-import styles from './Portfolio.module.scss';
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import image1 from "../../../public/images/portfolio/image1.png";
+import image2 from "../../../public/images/portfolio/image2.jpg";
+import image3 from "../../../public/images/portfolio/image3.jpg";
+import image4 from "../../../public/images/portfolio/image4.jpg";
+import styles from "./Portfolio.module.scss";
 
 const showStatus = {
   hide: styles.hide,
@@ -14,43 +14,33 @@ const showStatus = {
 
 const Portfolio: React.FC = (): JSX.Element => {
   const [selectedImage, setSelectedImage] = useState(0);
-  const [show, setShow] = useState('');
+  const [show, setShow] = useState("");
 
   const portfolioData = [
     {
-      text: `Влаштування рулонного газону с. Гатне`,
+      text: `Перегляньте деякі з наших минулих проектів ландшафтного дизайну.`,
       image: image1,
-      alt: 'About image 1',
+      alt: "About image 1",
     },
     {
-      text: `Комплексний благоустрій с. Гаврилівка.`,
+      text: `Перегляньте деякі з наших минулих проектів ландшафтного дизайну.`,
       image: image2,
-      alt: 'About image 2',
+      alt: "About image 2",
     },
     {
-      text: `Комплексний благоустрій с. Забуччя.`,
+      text: `Перегляньте деякі з наших минулих проектів ландшафтного дизайну.`,
       image: image3,
-      alt: 'About image 3',
+      alt: "About image 3",
     },
     {
-      text: `Комплексний благоустрій с. Катюжанка.`,
+      text: `Перегляньте деякі з наших минулих проектів ландшафтного дизайну.`,
       image: image4,
-      alt: 'About image 4',
-    },
-    {
-      text: `Комплексний благоустрій с. Тарасівка.`,
-      image: image4,
-      alt: 'About image 4',
-    },
-    {
-      text: `Комплексний благоустрій с. Шевченкове.`,
-      image: image4,
-      alt: 'About image 4',
+      alt: "About image 4",
     },
   ];
 
   const checkActive = (id: number): string =>
-    selectedImage === id ? `font-bold relative ${styles.active}` : '';
+    selectedImage === id ? `font-bold relative ${styles.active}` : "";
 
   const changeElement = (): void => {
     if (show === showStatus.hide) return;
@@ -79,16 +69,15 @@ const Portfolio: React.FC = (): JSX.Element => {
 
   return (
     <section className={styles.container} id="portfolio">
-      {/* xl:container px-5 mx-auto md:px-0 py-10 xl:pt-24 xl:pb-28 grid md:grid-cols-2 */}
-      <div className="mx-auto">
-        <h2 className="font-bold text-5xl mb-14 px-10">Наша робота</h2>
-        <div className="">
-          <div className="">
-            <p
+      <div className="xl:container px-5 mx-auto md:px-0 py-10 xl:pt-24 xl:pb-28 grid md:grid-cols-2">
+        <div className="flex flex-col justify-between md:py-10 md:ml-10 z-10">
+          <div className="md:mr-10">
+            <p className="text-2xl">Наша робота</p>
+            <h2
               className={`text-2xl md:text-xl lg:text-2xl xl:text-3xl h-48 mt-7 ${show}`}
             >
               {portfolioData[selectedImage].text}
-            </p>
+            </h2>
           </div>
           <button
             className={`text-xl w-24 uppercase text-left pb-1 md:visible invisible ${styles.button}`}
@@ -113,7 +102,7 @@ const Portfolio: React.FC = (): JSX.Element => {
                   onClick={() => selectElement(index)}
                   className={`${styles.numeric} ${checkActive(index)}`}
                 >
-                  {index >= 9 ? '' : 0}
+                  {index >= 9 ? "" : 0}
                   {index + 1}
                 </div>
               )
