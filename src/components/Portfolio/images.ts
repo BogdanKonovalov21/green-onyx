81cbcc7e7b97f442ed59fc2a5b66c21a1798b650
BogdanKonovalov21 <bogdan.k@devstaff.net>
2023-06-23 22:58:09 +0300

    path fix test 5

--- src/components/Portfolio/images.ts
+++ src/components/Portfolio/images.ts
@@ -1,30 +1,30 @@
-import firstGatne from '../../../public/images/portfolio/gatne/firstGatne.jpg';
-import secondGatne from '../../../public/images/portfolio/gatne/secondGatne.jpg';
-import thirdGatne from '../../../public/images/portfolio/gatne/thirdGatne.jpg';
-import fourthGatne from '../../../public/images/portfolio/gatne/fourthGatne.jpg';
+import firstGatne from '../../../public/portfolio/gatne/firstGatne.jpg';
+import secondGatne from '../../../public/portfolio/gatne/secondGatne.jpg';
+import thirdGatne from '../../../public/portfolio/gatne/thirdGatne.jpg';
+import fourthGatne from '../../../public/portfolio/gatne/fourthGatne.jpg';
 
-import firstTarasivka from '../../../public/images/portfolio/tarasivka/firstTarasivka.jpg';
-import secondTarasivka from '../../../public/images/portfolio/tarasivka/secondTarasivka.jpg';
-import thirdTarasivka from '../../../public/images/portfolio/tarasivka/thirdTarasivka.jpg';
-import fourthTarasivka from '../../../public/images/portfolio/tarasivka/fourthTarasivka.jpg';
+import firstTarasivka from '../../../public/portfolio/tarasivka/firstTarasivka.jpg';
+import secondTarasivka from '../../../public/portfolio/tarasivka/secondTarasivka.jpg';
+import thirdTarasivka from '../../../public/portfolio/tarasivka/thirdTarasivka.jpg';
+import fourthTarasivka from '../../../public/portfolio/tarasivka/fourthTarasivka.jpg';
 
-import firstShevchenkove from '../../../public/images/portfolio/shevchenkove/firstShevchenkove.jpg';
-import secondShevchenkove from '../../../public/images/portfolio/shevchenkove/secondShevchenkove.jpg';
-import thirdShevchenkove from '../../../public/images/portfolio/shevchenkove/thirdShevchenkove.jpg';
-import fourthShevchenkove from '../../../public/images/portfolio/shevchenkove/fourthShevchenkove.jpg';
+import firstShevchenkove from '../../../public/portfolio/shevchenkove/firstShevchenkove.jpg';
+import secondShevchenkove from '../../../public/portfolio/shevchenkove/secondShevchenkove.jpg';
+import thirdShevchenkove from '../../../public/portfolio/shevchenkove/thirdShevchenkove.jpg';
+import fourthShevchenkove from '../../../public/portfolio/shevchenkove/fourthShevchenkove.jpg';
 
-import firstKatyzhanka from '../../../public/images/portfolio/katyzhanka/firstKatyzhanka.jpg';
-import secondKatyzhanka from '../../../public/images/portfolio/katyzhanka/secondKatyzhanka.jpg';
-import thirdKatyzhanka from '../../../public/images/portfolio/katyzhanka/thirdKatyzhanka.jpg';
-import fourthKatyzhanka from '../../../public/images/portfolio/katyzhanka/fourthKatyzhanka.jpg';
+import firstKatyzhanka from '../../../public/portfolio/katyzhanka/firstKatyzhanka.jpg';
+import secondKatyzhanka from '../../../public/portfolio/katyzhanka/secondKatyzhanka.jpg';
+import thirdKatyzhanka from '../../../public/portfolio/katyzhanka/thirdKatyzhanka.jpg';
+import fourthKatyzhanka from '../../../public/portfolio/katyzhanka/fourthKatyzhanka.jpg';
 
-import firstZabutcha from '../../../public/images/portfolio/zabutcha/firstZabutcha.jpg';
-import secondZabutcha from '../../../public/images/portfolio/zabutcha/secondZabutcha.jpg';
+import firstZabutcha from '../../../public/portfolio/zabutcha/firstZabutcha.jpg';
+import secondZabutcha from '../../../public/portfolio/zabutcha/secondZabutcha.jpg';
 
-import firstGavrilivka from '../../../public/images/portfolio/gavrilivka/firstGavrilivka.jpg';
-import secondGavrilivka from '../../../public/images/portfolio/gavrilivka/secondGavrilivka.jpg';
-import thirdGavrilivka from '../../../public/images/portfolio/gavrilivka/thirdGavrilivka.jpg';
-import fourthGavrilivka from '../../../public/images/portfolio/gavrilivka/fourthGavrilivka.jpg';
+import firstGavrilivka from '../../../public/portfolio/gavrilivka/firstGavrilivka.jpg';
+import secondGavrilivka from '../../../public/portfolio/gavrilivka/secondGavrilivka.jpg';
+import thirdGavrilivka from '../../../public/portfolio/gavrilivka/thirdGavrilivka.jpg';
+import fourthGavrilivka from '../../../public/portfolio/gavrilivka/fourthGavrilivka.jpg';
 
 export const gatneImages = [firstGatne, secondGatne, thirdGatne, fourthGatne];
 export const tarasivkaImages = [
