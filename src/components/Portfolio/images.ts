d0ad5a10bf65e37637b5e9180e402fbb81eeaf35
BogdanKonovalov21 <bogdan.k@devstaff.net>
2023-06-23 23:04:24 +0300

    path fix test 6

--- src/components/Portfolio/images.ts
+++ src/components/Portfolio/images.ts
@@ -1,30 +1,30 @@
-import firstGatne from '../../../public/portfolio/gatne/firstGatne.jpg';
-import secondGatne from '../../../public/portfolio/gatne/secondGatne.jpg';
-import thirdGatne from '../../../public/portfolio/gatne/thirdGatne.jpg';
-import fourthGatne from '../../../public/portfolio/gatne/fourthGatne.jpg';
+import firstGatne from '../../images/portfolio/gatne/firstGatne.jpg';
+import secondGatne from '../../images/portfolio/gatne/secondGatne.jpg';
+import thirdGatne from '../../images/portfolio/gatne/thirdGatne.jpg';
+import fourthGatne from '../../images/portfolio/gatne/fourthGatne.jpg';
 
-import firstTarasivka from '../../../public/portfolio/tarasivka/firstTarasivka.jpg';
-import secondTarasivka from '../../../public/portfolio/tarasivka/secondTarasivka.jpg';
-import thirdTarasivka from '../../../public/portfolio/tarasivka/thirdTarasivka.jpg';
-import fourthTarasivka from '../../../public/portfolio/tarasivka/fourthTarasivka.jpg';
+import firstTarasivka from '../../images/portfolio/tarasivka/firstTarasivka.jpg';
+import secondTarasivka from '../../images/portfolio/tarasivka/secondTarasivka.jpg';
+import thirdTarasivka from '../../images/portfolio/tarasivka/thirdTarasivka.jpg';
+import fourthTarasivka from '../../images/portfolio/tarasivka/fourthTarasivka.jpg';
 
-import firstShevchenkove from '../../../public/portfolio/shevchenkove/firstShevchenkove.jpg';
-import secondShevchenkove from '../../../public/portfolio/shevchenkove/secondShevchenkove.jpg';
-import thirdShevchenkove from '../../../public/portfolio/shevchenkove/thirdShevchenkove.jpg';
-import fourthShevchenkove from '../../../public/portfolio/shevchenkove/fourthShevchenkove.jpg';
+import firstShevchenkove from '../../images/portfolio/shevchenkove/firstShevchenkove.jpg';
+import secondShevchenkove from '../../images/portfolio/shevchenkove/secondShevchenkove.jpg';
+import thirdShevchenkove from '../../images/portfolio/shevchenkove/thirdShevchenkove.jpg';
+import fourthShevchenkove from '../../images/portfolio/shevchenkove/fourthShevchenkove.jpg';
 
-import firstKatyzhanka from '../../../public/portfolio/katyzhanka/firstKatyzhanka.jpg';
-import secondKatyzhanka from '../../../public/portfolio/katyzhanka/secondKatyzhanka.jpg';
-import thirdKatyzhanka from '../../../public/portfolio/katyzhanka/thirdKatyzhanka.jpg';
-import fourthKatyzhanka from '../../../public/portfolio/katyzhanka/fourthKatyzhanka.jpg';
+import firstKatyzhanka from '../../images/portfolio/katyzhanka/firstKatyzhanka.jpg';
+import secondKatyzhanka from '../../images/portfolio/katyzhanka/secondKatyzhanka.jpg';
+import thirdKatyzhanka from '../../images/portfolio/katyzhanka/thirdKatyzhanka.jpg';
+import fourthKatyzhanka from '../../images/portfolio/katyzhanka/fourthKatyzhanka.jpg';
 
-import firstZabutcha from '../../../public/portfolio/zabutcha/firstZabutcha.jpg';
-import secondZabutcha from '../../../public/portfolio/zabutcha/secondZabutcha.jpg';
+import firstZabutcha from '../../images/portfolio/zabutcha/firstZabutcha.jpg';
+import secondZabutcha from '../../images/portfolio/zabutcha/secondZabutcha.jpg';
 
-import firstGavrilivka from '../../../public/portfolio/gavrilivka/firstGavrilivka.jpg';
-import secondGavrilivka from '../../../public/portfolio/gavrilivka/secondGavrilivka.jpg';
-import thirdGavrilivka from '../../../public/portfolio/gavrilivka/thirdGavrilivka.jpg';
-import fourthGavrilivka from '../../../public/portfolio/gavrilivka/fourthGavrilivka.jpg';
+import firstGavrilivka from '../../images/portfolio/gavrilivka/firstGavrilivka.jpg';
+import secondGavrilivka from '../../images/portfolio/gavrilivka/secondGavrilivka.jpg';
+import thirdGavrilivka from '../../images/portfolio/gavrilivka/thirdGavrilivka.jpg';
+import fourthGavrilivka from '../../images/portfolio/gavrilivka/fourthGavrilivka.jpg';
 
 export const gatneImages = [firstGatne, secondGatne, thirdGatne, fourthGatne];
 export const tarasivkaImages = [
